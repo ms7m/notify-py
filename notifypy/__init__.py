dbe84ff7326589980f8841d93b31280de4e0ff56
Mustafa Mohamed <ms7mohamed@gmail.com>
2020-05-18 10:43:35 -0400

    Bump to 0.0.7.
    - remove snoretoast binary

--- notifypy/__init__.py
+++ notifypy/__init__.py
@@ -1,3 +1,3 @@
 from .notify import Notify
 
-__version__ = "0.0.6"
+__version__ = "0.0.7"
--- setup.py
+++ setup.py
@@ -7,15 +7,27 @@ from setuptools import Command, find_packages, setup
 
 import notifypy
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
-    name="notifypy",
+    name="notify_py",
     version=notifypy.__version__,
     author="Mustafa Mohamed",
     author_email="ms7mohamed@gmail.com",
+    description="Cross-platform desktop notification library for Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ms7m/notify-py",
     python_requires=">=3.6.0",
     packages=find_packages(
         exclude=["testing", "*.testing", "*.testing.*", "testing.*"]
     ),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
     include_package_data=True,
     install_requires=[
         "loguru==0.4.1"

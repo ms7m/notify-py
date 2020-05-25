8cfea1a3536464b8015572125968865c77aa4153
Mustafa Mohamed <ms7mohamed@gmail.com>
2020-05-25 18:43:10 -0400

    Add custom exceptions in init

--- notifypy/__init__.py
+++ notifypy/__init__.py
@@ -1,3 +1,11 @@
 from .notify import Notify
+from .exceptions import (
+    UnsupportedPlatform,
+    InvalidAudioPath,
+    InvalidIconPath,
+    NotificationFailure,
+    BinaryNotFound
+)
+
 
 __version__ = "0.0.8"

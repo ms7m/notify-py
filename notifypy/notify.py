bfe42c6a2e25d41cdc4d5820eaada60ba49c41b9
Mustafa Mohamed <ms7mohamed@gmail.com>
2020-07-05 21:45:38 -0400

    return str of audio path only if it's not None

--- notifypy/notify.py
+++ notifypy/notify.py
@@ -281,7 +281,9 @@ class Notify:
                 notification_subtitle=str(supplied_message),
                 application_name=str(supplied_application_name),
                 notification_icon=str(supplied_icon_path),
-                notification_audio=supplied_audio_path,
+                notification_audio=str(supplied_audio_path)
+                if supplied_audio_path
+                else None,
             )
             if attempt_to_send_notifiation:
                 logger.info("Sent notification.")
